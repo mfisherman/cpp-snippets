074366f49dfc101c89acbbbbe034cbcb7c9ae3b3
Marc Fischer <mfischer@student.ethz.ch>
2021-08-23 20:11:58 +0200

    More examples for modulo

--- modulo_main.cpp
+++ modulo_main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <assert.h>
+
 // In C/C++, the modulo operator "%"" is actually computing the remainder - contrarily to
 // the mathematical euclidean remainder (also known as euclidean division, see https://en.wikipedia.org/wiki/Euclidean_division).
 // Indeed, there are three main ways to compute the remainder: see https://en.wikipedia.org/wiki/Modulo_operation#Variants_of_the_definition.
@@ -41,8 +42,8 @@ int64_t modulo_truncated(int64_t a, int64_t b)
 // Due to the floor function, the quotient is always rounded downwards, even if it is already negative.
 // The remainder is thus given by:
 // r = a − b*⌊a/b⌋
-// From: https://codereview.stackexchange.com/q/253799
 // Undefined behaviour if b is 0.
+// From: https://codereview.stackexchange.com/q/253799
 int64_t modulo_floored(int64_t a, int64_t b)
 {
     assert(b != 0);
@@ -57,7 +58,7 @@ int64_t modulo_floored(int64_t a, int64_t b)
 // 21 euclidean modulo 4 is 1
 // -21 euclidean modulo 4 is 3
 // Undefined behaviour if b is 0.
-// From https://stackoverflow.com/a/20638659.
+// From: https://stackoverflow.com/a/20638659.
 int64_t euclidean_modulo(int64_t a, int64_t b)
 {
     assert(b != 0);
@@ -73,6 +74,22 @@ int64_t euclidean_modulo(int64_t a, int64_t b)
     return m;
 }
 
+// This function computes the euclidean reminder for values where b > 0.
+int64_t euclidean_modulo_natural(int64_t a, int64_t b)
+{
+    assert(b > 0);
+    return ((a % b) + b) % b;
+}
+
+// This function computes the euclidean reminder for values where a >= 0 and b > 0.
+// Due to the precondition, the euclidean reminder is the same as just using modulo.
+inline uint64_t euclidean_modulo_all_natural(uint64_t a, uint64_t b)
+{
+    assert(a >= 0);
+    assert(b > 0);
+    return a % b;
+}
+
 int main(int argc, char **argv)
 {
     std::cout << "C/C++ modulo (aka. remainder): " << std::endl;
@@ -81,6 +98,8 @@ int main(int argc, char **argv)
     std::cout << "modulo_truncated(21, -4) = " << modulo_truncated(21, -4) << std::endl;
     std::cout << "modulo_truncated(100, -1) = " << modulo_truncated(100, -1) << std::endl;
     std::cout << "modulo_truncated(INT64_MIN, -1) = " << modulo_truncated(INT64_MIN, -1) << std::endl;
+    std::cout << "modulo_truncated(INT64_MIN, 68488) = " << modulo_truncated(INT64_MIN, 68488) << std::endl;
+    std::cout << "modulo_truncated(INT64_MAX, 76953) = " << modulo_truncated(INT64_MAX, 76953) << std::endl;
 
     std::cout << "Modulo floored: " << std::endl;
     std::cout << "modulo_floored(21, 4) = " << modulo_floored(21, 4) << std::endl;
@@ -88,6 +107,8 @@ int main(int argc, char **argv)
     std::cout << "modulo_floored(21, -4) = " << modulo_floored(21, -4) << std::endl;
     std::cout << "modulo_floored(100, -1) = " << modulo_floored(100, -1) << std::endl;
     std::cout << "modulo_floored(INT64_MIN, -1) = " << modulo_floored(INT64_MIN, -1) << std::endl;
+    std::cout << "modulo_floored(INT64_MIN, 68488) = " << modulo_floored(INT64_MIN, 68488) << std::endl;
+    std::cout << "modulo_floored(INT64_MAX, 76953) = " << modulo_floored(INT64_MAX, 76953) << std::endl;
 
     std::cout << "Mathematical euclidean modulo: " << std::endl;
     std::cout << "euclidean_modulo(21, 4) = " << euclidean_modulo(21, 4) << std::endl;
@@ -95,5 +116,17 @@ int main(int argc, char **argv)
     std::cout << "euclidean_modulo(21, -4) = " << euclidean_modulo(21, -4) << std::endl;
     std::cout << "euclidean_modulo(100, -1) = " << euclidean_modulo(100, -1) << std::endl;
     std::cout << "euclidean_modulo(INT64_MIN, -1) = " << euclidean_modulo(INT64_MIN, -1) << std::endl;
+    std::cout << "euclidean_modulo(INT64_MIN, 68488) = " << euclidean_modulo(INT64_MIN, 68488) << std::endl;
+    std::cout << "euclidean_modulo(INT64_MAX, 76953) = " << euclidean_modulo(INT64_MAX, 76953) << std::endl;
+
+    std::cout << "Mathematical euclidean modulo (only positive b): " << std::endl;
+    std::cout << "euclidean_modulo_natural(21, 4) = " << euclidean_modulo_natural(21, 4) << std::endl;
+    std::cout << "euclidean_modulo_natural(-21, 4) = " << euclidean_modulo_natural(-21, 4) << std::endl;
+    std::cout << "euclidean_modulo_natural(INT64_MIN, 68488) = " << euclidean_modulo_natural(INT64_MIN, 68488) << std::endl;
+    std::cout << "euclidean_modulo_natural(INT64_MAX, 76953) = " << euclidean_modulo_natural(INT64_MAX, 76953) << std::endl;
+
+    std::cout << "Mathematical euclidean modulo (only positive values): " << std::endl;
+    std::cout << "euclidean_modulo_all_natural(21, 4) = " << euclidean_modulo_natural(21, 4) << std::endl;
+    std::cout << "euclidean_modulo_all_natural(INT64_MAX, 76953) = " << euclidean_modulo_natural(INT64_MAX, 76953) << std::endl;
     return 0;
 }
